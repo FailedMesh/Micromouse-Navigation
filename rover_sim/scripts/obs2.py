370e3319da30d53264e84dbf84d839d4bef8379c
sadha12 <81227060+sadha12@users.noreply.github.com>
2021-07-24 20:15:14 +0530

    Update obs2.py

--- rover_sim/scripts/obs2.py
+++ rover_sim/scripts/obs2.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env python3
 
+from numpy.core.fromnumeric import take
 import rospy
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist, Point
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan 
 from tf.transformations import euler_from_quaternion
-from math import atan2
+from math import atan2, cos, pi, sin, sqrt
+import roslaunch
 
 class class1(object):
     def __init__(self):
@@ -37,43 +39,91 @@ class class1(object):
         x_err = goal.x - self.curr_x
         y_err = goal.y -self.curr_y
         angle_to_goal = atan2(y_err, x_err)
+        d_goal = sqrt((x_err*x_err) + (y_err*y_err))
 
 #If the distance to an obstacle in front of the robot is bigger than 1 meter, the robot will move forward
-        if (msg.ranges[300] > self.disToObstacle) and (self.bt > 55):
+        if (msg.ranges[1] > self.disToObstacle) and (self.bt > 55):
             self.move.linear.x = 0.2
             self.move.angular.z = 0.0
 
 #If the distance to an obstacle in front of the robot is smaller than 1 meter, the robot will turn left
-        if (msg.ranges[300] <= self.disToObstacle) and (self.bt > 55): 
+        if ((msg.ranges[1] <= self.disToObstacle) and (self.bt > 55)) or ((msg.ranges[355] <= self.disToObstacle) and (self.bt > 55)) or ((msg.ranges[6] <= self.disToObstacle) and (self.bt > 55)): 
             self.move.linear.x = 0.0
             self.move.angular.z = 0.5
             rospy.loginfo(" Battery remaining is -  %s",self.bt)
 
-        if (msg.ranges[300] > self.disToObstacle) and (self.bt >= 55):
+        if (msg.ranges[1] > self.disToObstacle) and (self.bt > 55):
             self.move.linear.x = 0.2
             self.move.angular.z = 0.0
-            rospy.loginfo(" Battery remaining is -  %s",self.bt)
-
-        if (abs(angle_to_goal - self.theta)>0.1) and (self.bt < 55) and (self.bt > 5): 
-            self.move.linear.x = 0.0
-            self.move.angular.z = 0.5
             rospy.loginfo(" Battery remaining is -  %s",self.bt)
             
 
-        if (abs(angle_to_goal - self.theta)<=0.1) and (abs(x_err)>0.1) and (self.bt < 55) and (self.bt > 5): 
-            self.move.linear.x = 0.2
-            self.move.angular.z = 0.0
-            rospy.loginfo("Returning to origin")
+        if (self.bt <= 55) and (self.bt > 5): 
+            goal = Point()
+            goal.x = 0
+            goal.y = 0
+            x_err = goal.x - self.curr_x
+            y_err = goal.y -self.curr_y
+            angle_to_goal = atan2(y_err, x_err)
+            d_goal = sqrt((x_err*x_err) + (y_err*y_err))
+            min_value = min(msg.ranges)
+            min_index = msg.ranges.index(min_value)
+            d_obstacle = 0
+            theta_obstacle = 0
+            
+            theta_goal = angle_to_goal
+            if min_value>0 and min_value<=0.5 and min_index <=180:
+                theta_obstacle = min_index*pi/180
+                d_obstacle = min_value
+            if min_value>0 and min_value<=0.5 and min_index >180:
+                theta_obstacle = (min_index-360)*pi/180
+                d_obstacle = min_value
 
-        if (self.bt <= 5 and (abs(msg.ranges[300])>0)) or (self.bt <= 55 and abs(x_err < 0.1)):
+            rospy.loginfo(" Battery remaining is -  %s",self.bt)
+            # rospy.loginfo(" Dist to obstacle %s",d_obstacle)
+            phi = atan2(10*d_obstacle*sin(pi+theta_obstacle),(d_goal + (10*d_obstacle*sin(pi+theta_obstacle))))
+            #rospy.loginfo(" Phi %s",phi)
+
+            theta2 = phi + angle_to_goal
+
+            if d_goal>0.1 and abs(theta2-self.theta) >= 0.2:
+                self.move.angular.z = 0.5*(theta2-self.theta)
+                self.move.linear.x = 0.2
+
+            if d_goal>0.1 and abs(theta2-self.theta) < 0.2:
+                self.move.angular.z = 0
+                self.move.linear.x = 0.2
+            
+            if d_goal <=0.1:
+                self.move.linear.x = 0.0
+                self.move.angular.z = 0
+
+            
+            # rospy.loginfo('Returning to origin.. Autonomous navigation node up and running')
+            # package = 'rover_srv'
+            # executable = 'trt.py'
+            # node = roslaunch.core.Node(package, executable)
+
+            # launch = roslaunch.scriptapi.ROSLaunch()
+            # launch.start()
+
+            # task = launch.launch(node)
+           
+            
+        if (self.bt) <= 5 or ((self.bt) <= 55 and d_goal <=0.1):
             self.move.linear.x = 0
             self.move.angular.z = 0
             rospy.loginfo(" Battery remaining is -  %s",self.bt)
+
+        if ((self.bt) <= 55 and d_goal <=0.1):
+            self.move.linear.x = 0
+            self.move.angular.z = 0
+            rospy.loginfo(" Reached the charging check point -  %s",self.bt)
             
 
         pub.publish(self.move)
         
-        rate.sleep()
+        
 
 
     
